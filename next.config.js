1359415420f252fb5c27a66519e0502674f5d5cd
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-06-22 05:38:14 +0100

    fix: update next.config.js images remote patterns

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'pbxt.replicate.delivery',
-      'g4yqcv8qdhf169fk.public.blob.vercel-storage.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.replicate.delivery',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
